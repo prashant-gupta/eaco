af47530e69d2623dd3eb4064c85b5653aa57c153
Prashant <pgupta.se@gmail.com>
2013-06-16 23:39:51 +0530

    add functionality to get ant with minimum timeDuration and with all activities processed.

--- MavenEACO/src/main/java/Main.java
+++ MavenEACO/src/main/java/Main.java
@@ -14,11 +14,14 @@ import java.util.*;
 
 import static java.lang.System.out;
 public class Main {
+
     static final int MAX_ACTIVITIES=10;
     static Set<Activity> activitySet=new LinkedHashSet<Activity>();
     static List<Employee> employeeList=new ArrayList<Employee>();
-    static List<Thread> antList=new ArrayList<Thread>();
+    static List<Thread> antThreadList =new ArrayList<Thread>();
+    static List<Ant> antList=new ArrayList<Ant>();
     static Map<Set<Employee>,Double> pathMap;
+
 
     public static void main(String... args){
         generateActivities();
@@ -31,11 +34,11 @@ public class Main {
             out.println();
         }
         out.println("##############");
-        out.println("Ant list: "+antList);
+        out.println("Ant list: "+ antThreadList);
         out.println("Activity Universe: "+activitySet);
         out.println("##############");
 
-        for (Thread a:antList){
+        for (Thread a: antThreadList){
             a.start();
             try{
                 a.join();
@@ -44,6 +47,9 @@ public class Main {
                 out.println("Exception ================ "+t.getMessage());
             }
         }
+        Collections.sort(antList,new Ant.OrderByTimeDuration());
+        out.println("==== "+antList);
+        out.println("==== "+antList.get(0).getAntPathMap());
     }
 
     public static void generateActivities(){
@@ -72,7 +78,7 @@ public class Main {
                 employee.addActivity((Activity)a);
             }
 
-            String time=JOptionPane.showInputDialog(null,"Enter total number of Employees","EACO",JOptionPane.QUESTION_MESSAGE);
+            String time=JOptionPane.showInputDialog(null,"Enter total time taken","EACO",JOptionPane.QUESTION_MESSAGE);
             Double totalTime=null;
             try{totalTime=Double.parseDouble(time);}
             catch(Exception e){out.println(e.getMessage());System.exit(1);}
@@ -94,7 +100,9 @@ public class Main {
     public static void populateAnts(){
         int i=0;
         for (Employee e:employeeList){
-            antList.add(new Thread(new Ant(employeeList,employeeList.get(i),"Ant"+i),"Ant"+i));
+            Ant ant=new Ant(employeeList, employeeList.get(i), "Ant" + i, activitySet);
+            antThreadList.add(new Thread(ant, "Ant" + i));
+            antList.add(ant);
             i++;
         }
     }
@@ -108,6 +116,21 @@ public class Main {
                     keySet.add(e1);
                     keySet.add(e2);
                     map.put(keySet,0.0);
+                }
+            }
+        }
+        return map;
+    }
+
+    public static Map<Set<Employee>,Boolean> generatePathMapForAnt(Set <Employee> employeeSet){
+        Map<Set<Employee>,Boolean> map=new LinkedHashMap<Set<Employee>, Boolean>();
+        for ( Employee e1: employeeSet){
+            for (Employee e2: employeeSet){
+                if(!e2.equals(e1)){
+                    Set<Employee> keySet=new LinkedHashSet<Employee>();
+                    keySet.add(e1);
+                    keySet.add(e2);
+                    map.put(keySet,Boolean.FALSE);
                 }
             }
         }
--- MavenEACO/src/main/java/org/prashant/eaco/Ant.java
+++ MavenEACO/src/main/java/org/prashant/eaco/Ant.java
@@ -1,8 +1,6 @@
 package org.prashant.eaco;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,36 +11,108 @@ import java.util.List;
  */
 import static java.lang.System.out;
 public class Ant implements Runnable{
-    ArrayList<Employee> employeeList;
-    String name;
-    Employee start;
+    private ArrayList<Employee> employeeList;
+    private Set<Activity> processedActivities=new LinkedHashSet<Activity>();
+    private Set<Activity> totalActivities=new LinkedHashSet<Activity>();
+    private String name;
+    private Employee start;
+    private Map<Set<Employee>,Boolean> antPathMap;
+    private List<Employee> currentEmployeePair=new ArrayList<Employee>();
+    private Long timeDuration;
 
-    public Ant(List<Employee> employeeList,Employee start,String name){
+    public Ant(List<Employee> employeeList,Employee start,String name,Set<Activity> totalActivities){
         this.employeeList=new ArrayList<Employee>(employeeList);
         this.start=start;
         this.name=name;
+        this.setAntPathMap(generatePathMapForAnt(new LinkedHashSet<Employee>(employeeList)));
+        this.totalActivities=totalActivities;
+    }
+
+    public Long getTimeDuration() {
+        return timeDuration;
+    }
+
+    public void setTimeDuration(Long timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public Map<Set<Employee>, Boolean> getAntPathMap() {
+        return this.antPathMap;
+    }
+
+    public void setAntPathMap(Map<Set<Employee>, Boolean> antPathMap) {
+        this.antPathMap = antPathMap;
     }
 
     public void run(){
         out.print("Ant: "+this.name);
         Collections.shuffle(employeeList);
-        out.print("||Employee: "+this.start+"||Activities: " +this.start.getActivitySet().size() + "||TTime: " + this.start.getTotalTime());
+        Long startTime=System.nanoTime();
+        out.print(" || Employee: "+this.start+"|Activities: " +this.start.getActivitySet() + "|TTime: " + this.start.getTotalTime());
         employeeList.remove(this.start);
+        currentEmployeePair.add(this.start);
+        processedActivities.addAll(this.start.getActivitySet());
 
         /*
         * Diving into the recursion call :-)
         * */
 
-         walkAnt(employeeList.size()>0);
-
+        walkAnt(employeeList.size() > 0);
+        Long endTime=System.nanoTime();
+        this.setTimeDuration(endTime-startTime);
         out.println();
     }
 
     public void walkAnt(boolean flag){
         if (flag){
             Employee e=employeeList.remove(0);
-            out.print("||Employee: "+e+"||Activities: " + e.getActivitySet().size() + "||TTime: " + e.getTotalTime());
-            walkAnt(employeeList.size()>0);
+            out.print(" || Employee: "+e+"|Activities: " + e.getActivitySet()+ "|TTime: " + e.getTotalTime());
+            currentEmployeePair.add(e);
+            updateCurrentEmployeePairPath(currentEmployeePair, antPathMap);
+            processedActivities.addAll(e.getActivitySet());
+            if (processedActivities.containsAll(totalActivities)) return;
+            walkAnt(employeeList.size() > 0);
+        }
+    }
+
+    public static Map<Set<Employee>,Boolean> generatePathMapForAnt(Set <Employee> employeeSet){
+        Map<Set<Employee>,Boolean> map=new LinkedHashMap<Set<Employee>, Boolean>();
+        for ( Employee e1: employeeSet){
+            for (Employee e2: employeeSet){
+                if(!e2.equals(e1)){
+                    Set<Employee> keySet=new LinkedHashSet<Employee>();
+                    keySet.add(e1);
+                    keySet.add(e2);
+                    map.put(keySet,Boolean.FALSE);
+                }
+            }
+        }
+        return map;
+    }
+
+    public void updateCurrentEmployeePairPath(List<Employee> employeePairList,Map<Set<Employee>,Boolean> antPathMap){
+        if (employeePairList.size()>=2){
+            Set<Employee> employeeSet=new LinkedHashSet<Employee>(employeePairList);
+            if (antPathMap.containsKey(employeeSet)){
+                antPathMap.put(employeeSet,Boolean.TRUE);
+            }
+            employeePairList.remove(0);
+        }
+    }
+
+    public String toString(){
+        return this.name+" "+timeDuration;
+    }
+
+    public static class OrderByTimeDuration implements Comparator<Ant>{
+
+        public int compare(Ant a1,Ant a2){
+            if (a1.getTimeDuration().equals(a2.getTimeDuration())){
+                return 0;
+            }
+            else{
+                return (a1.getTimeDuration()>a2.getTimeDuration())?1:-1;
+            }
         }
     }
 }
